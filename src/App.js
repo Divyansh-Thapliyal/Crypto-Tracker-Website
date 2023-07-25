85ad029658ba9310b4c660be6c6b1b51a72ff5cb
Divyansh-Thapliyal <divyanshthapliyalwork007@gmail.com>
2023-07-25 07:15:10 +0530

    Coins Searching form done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 
-import 'App.css';
+import {useState,useEffect} from 'react';
+import './App.css';
 import axios from 'axios';
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en
+ const api='https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en';
 
 function App() {
+
+  const [coins,setCoins]=useState([]);
+
+  useEffect(()=>{
+
+     axios.get(api).then((res)=>{
+      console.log(res.data);
+      setCoins(res.data);
+     }).catch((err)=>{console.log(err)});
+
+  },[]);
+
+
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a CryptoCurrency.</h1>
+        <form>
+          <input type="text" placeholder='Search' className='coin-input'/>
+        </form>
+      </div>
     </div>
   );
 }
