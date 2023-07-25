35df4340c10316fc8b786d96e55499159a0e3c21
Divyansh-Thapliyal <divyanshthapliyalwork007@gmail.com>
2023-07-25 07:03:45 +0530

    Getting Api link done.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+
+import 'App.css';
+import axios from 'axios';
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en
+
 function App() {
   return (
     <div>
